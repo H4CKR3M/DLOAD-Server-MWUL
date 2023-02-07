f4832a2187bfdf2f2ebf55ccfd68e03043582ae6
H4CKR3M <121751918+H4CKR3M@users.noreply.github.com>
2023-02-07 15:40:25 -0800

    Changed URL

--- Server.js
+++ Server.js
@@ -3,6 +3,6 @@ var SERVER = {
     MAP_VERSION: 1,
     DAILIES: [],
     INIT: function INIT(){
-        DLoad.DownloadContent("https://dl.dropboxusercontent.com/s/g94z5ptu9v6rmm2/Soundtrack.zip");
+        DLoad.DownloadContent("https://dl.dropboxusercontent.com/s/0pylpe3fq1w41sb/Soundtrack.zip");
     }
 }
